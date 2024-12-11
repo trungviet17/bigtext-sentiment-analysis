3b8829fd75c0e5854d658444d0e7626506bc9ff3
Trung Viet <113108053+trungviet17@users.noreply.github.com>
2024-12-11 13:56:49 +0000

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='big-sentiment-model',
+    version='0.1',
+    packages=find_packages(),  # Automatically discover sub-packages
+    install_requires=[],  # Add dependencies here, e.g., ['numpy', 'pandas']
+    include_package_data=True,
+
+)
